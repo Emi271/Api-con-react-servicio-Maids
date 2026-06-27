da87a277b67aac252f96f718acc2106d94aca293
EmixD <114432937+Emi271@users.noreply.github.com>
2026-06-27 02:45:08 +0000

    aplicación casi completa

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Ajusta el puerto al que estés usando en Spring Boot (generalmente 8080)
 export const api = axios.create({
-    baseURL: 'http://localhost:8080/api',
+    baseURL: 'https://ubiquitous-parakeet-qjj9gjvvwwwf9xvx-8080.app.github.dev/',
     headers: {
         'Content-Type': 'application/json'
     }
--- src/main/java/com/programacion4/apicompeja/ApicompejaApplication.java
+++ src/main/java/com/programacion4/apicompeja/ApicompejaApplication.java
@@ -2,6 +2,9 @@ package com.programacion4.apicompeja;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class ApicompejaApplication {
@@ -9,5 +12,8 @@ public class ApicompejaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApicompejaApplication.class, args);
 	}
-
+@Bean
+public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+}
 }
